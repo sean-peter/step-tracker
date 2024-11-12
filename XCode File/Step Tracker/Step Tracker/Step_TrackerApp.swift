fc8ae69a2098091dd5ba6d837eca134e4c8c8149
Sean Ryan <seanryan6000@gmail.com>
2024-11-12 18:52:44 +0000

    created xcode project
    created the initial project

---
+++ XCode File/Step Tracker/Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Sean Ryan on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
