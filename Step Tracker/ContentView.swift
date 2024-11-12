ead1c755ca396c9d039d3ac6f75073e81f6d89db
Sean Ryan <seanryan6000@gmail.com>
2024-11-12 21:46:38 +0000

    day-1-ui-test-commit
    ui built by self, testing the commit feature

--- Step Tracker/ContentView.swift
+++ Step Tracker/ContentView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+      
+        NavigationStack {
+            VStack(alignment: .leading) {
+                HStack {
+                            VStack {
+                                HStack {
+                                    
+                                    Image(systemName: "figure.walk")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(height: 30, alignment: .center)
+                                        .imageScale(.large)
+                                        .foregroundStyle(.tint)
+                                        
+                                    
+                                    Text("Steps")
+                                        .font(.title)
+                                        .fontWeight(.bold)
+                                        .foregroundStyle(.tint)
+                                
+                                }
+                               
+                                Text("Avg: 10K Steps")
+                                    .font(.footnote)
+                                    
+                
+                                
+                            }
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                            
+                          
+                    }
+                
+                    
+                    Rectangle()
+                        .frame(width: .infinity, height: 200, alignment: .center)
+                        .cornerRadius(20)
+                }
+            
+                .padding(15)
+                .background(Color.gray)
+            .cornerRadius(24)
+            .navigationTitle("hello")
         }
         .padding()
+
+        
     }
 }
 
