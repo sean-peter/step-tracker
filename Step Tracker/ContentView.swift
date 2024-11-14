cc91d23109e658b83756542354031e1a5df02acf
Sean Ryan <seanryan6000@gmail.com>
2024-11-14 21:30:15 +0000

    added picker

--- Step Tracker/ContentView.swift
+++ Step Tracker/ContentView.swift
@@ -7,33 +7,62 @@
 
 import SwiftUI
 
+
+
+enum HealthMetricContext: CaseIterable, Identifiable {
+    case steps, weight
+    
+    var id: Self { self }
+    var title: String {
+        switch self {
+        case.steps:
+            return "Steps"
+        case.weight:
+            return "Weight"
+        }
+    }
+    
+}
+
+
+
 struct ContentView: View {
+    
+    @State private var selectedStat: HealthMetricContext = .steps
+    
     var body: some View {
         
         NavigationStack{
             ScrollView{
-               
-                //MARK: - top card
-                
                 VStack(spacing: 20){
-                    VStack{
-                        HStack{
-                            VStack(alignment: .leading){
-                                Label("Steps", systemImage: "figure.walk")
-                                    .font(.title3.bold())
-                                    .foregroundStyle(.pink)
-                                
-                                
-                                Text("Avg: 10K Steps")
-                                    .font(.caption)
-                                    .foregroundStyle(.secondary)
-
-                            }
-                            
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                            
+                    
+                    Picker("Selected Stat", selection: $selectedStat) {
+                        ForEach(HealthMetricContext.allCases) { metric in
+                            Text(metric.title)
                         }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    VStack{
+                        NavigationLink(value: selectedStat){
+                            HStack{
+                                VStack(alignment: .leading){
+                                    Label("Steps", systemImage: "figure.walk")
+                                        .font(.title3.bold())
+                                        .foregroundStyle(.pink)
+                                    
+                                    
+                                    Text("Avg: 10K Steps")
+                                        .font(.caption)
+
+                                }
+                                
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                
+                            }
+                        }
+                        .foregroundStyle(.secondary)
                         .padding(.bottom, 12)
                         
                         RoundedRectangle(cornerRadius: 6)
@@ -88,6 +117,9 @@ struct ContentView: View {
             }
             .padding()
             .navigationTitle("Dashboard")
+            .navigationDestination(for: HealthMetricContext.self) { metric in
+                Text(metric.title)
+            }
         }
         
   
